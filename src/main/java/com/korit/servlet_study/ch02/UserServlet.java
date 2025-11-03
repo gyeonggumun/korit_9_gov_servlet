83688ce09b024d5e3a420441be69b4cad44bba19
gyeonggumun <lion989072@gmail.com>
2025-11-03 15:46:52 +0900

    post 문제 풀이

--- src/main/java/com/korit/servlet_study/ch02/UserServlet.java
+++ src/main/java/com/korit/servlet_study/ch02/UserServlet.java
@@ -1,4 +1,67 @@
 package com.korit.servlet_study.ch02;
 
-public class UserServlet {
+import lombok.AllArgsConstructor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@WebServlet("/ch2/users")
+public class UserServlet extends HttpServlet {
+
+    private List<User> users;
+
+    @Override
+    public void init()  {
+        users = new ArrayList<>();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+        // username == "test"
+        // 찾으면 User 객체 응답(200), 못찾으면 해당 username은 존재하지 않습니다.(404)
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+
+
+        }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        String email = req.getParameter("email");
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setName(name);
+        user.setEmail(email);
+
+//        User user = User.builder()
+//                .username(username)
+//                .password(password)
+//                .name(name)
+//                .email(email)
+//                .build();
+
+//        User user = new User(username, password, name, email);
+
+        users.add(user);
+        System.out.println(users);
+        resp.setStatus(HttpServletResponse.SC_OK);
+        resp.getWriter().println("사용자 등록 완료");
+
+    }
 }
