505d0750649a9577b476eee1ec97a0287b51b8de
gyeonggumun <lion989072@gmail.com>
2025-11-03 16:47:04 +0900

    get 문제 풀이

--- src/main/java/com/korit/servlet_study/ch02/UserServlet.java
+++ src/main/java/com/korit/servlet_study/ch02/UserServlet.java
@@ -10,10 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @WebServlet("/ch2/users")
 public class UserServlet extends HttpServlet {
@@ -26,12 +23,23 @@ public class UserServlet extends HttpServlet {
     }
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws UnsupportedEncodingException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         // username == "test"
         // 찾으면 User 객체 응답(200), 못찾으면 해당 username은 존재하지 않습니다.(404)
         req.setCharacterEncoding(StandardCharsets.UTF_8.name());
-
-
+        List<User> foundUsers = users.stream()
+                .filter(user -> user
+                        .getUsername().equals(req.getParameter("username")))
+                .toList();
+        User foundUser = foundUsers.isEmpty() ? null : foundUsers.get(0);
+        if (Objects.isNull(foundUser)) {
+            resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            resp.getWriter().println("해당 username은 존재하지 않습니다.");
+            return;
+        }
+        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.getWriter().println(foundUser);
 
         }
 
