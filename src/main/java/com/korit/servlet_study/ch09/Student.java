ad2fb427d3a2b90939f6b7dfe7151caadb8aa5bf
gyeonggumun <lion989072@gmail.com>
2025-11-06 14:23:10 +0900

    Repository 재활용

--- src/main/java/com/korit/servlet_study/ch09/Student.java
+++ src/main/java/com/korit/servlet_study/ch09/Student.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Student {
+    private int id;
     private String name;
     private int age;
     private String address;
--- src/main/java/com/korit/servlet_study/ch09/StudentServlet.java
+++ src/main/java/com/korit/servlet_study/ch09/StudentServlet.java
@@ -2,6 +2,7 @@ package com.korit.servlet_study.ch09;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,35 +17,28 @@ import java.util.Objects;
 
 @WebServlet("/ch09/students")
 public class StudentServlet extends HttpServlet {
-    List<Student> students = new ArrayList<>();
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private StudentRepository studentRepository;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        studentRepository = new StudentRepository();
+        config.getServletContext().setAttribute("sr", studentRepository);
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//        resp.setContentType("application/json");
-        ObjectMapper objectMapper = new ObjectMapper();
-        // 이름에 필터링을 거는 로직
+
         String searchNameValue = req.getParameter("searchName");
-        if (Objects.isNull(searchNameValue)) {
-            objectMapper.writeValue(resp.getWriter(), students);
-            return;
-        }
-        List<Student> foundStudents = students.stream()
-                .filter(student -> student.getName().contains(searchNameValue))
-                .toList();
-            objectMapper.writeValue(resp.getWriter(), foundStudents);
+        objectMapper.writeValue(resp.getWriter(), studentRepository.findAllBySearchNameValue(searchNameValue));
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//        req.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
-//        resp.setContentType("application/json");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        students.add(objectMapper.readValue(req.getReader(), Student.class));
-        System.out.println(students);
+        Student student = objectMapper.readValue(req.getReader(), Student.class);
+        studentRepository.insert(student);
 
         objectMapper.writeValue(resp.getWriter(), Map.of("message", "학생정보 추가 완료"));
 
