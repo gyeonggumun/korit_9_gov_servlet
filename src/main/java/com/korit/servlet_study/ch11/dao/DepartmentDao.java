45a3d029140fbce06692a312fdcbbca879d4b33b
gyeonggumun <lion989072@gmail.com>
2025-11-13 11:47:25 +0900

    DBConnectionMgr 설치

---
+++ src/main/java/com/korit/servlet_study/ch11/dao/DepartmentDao.java
@@ -0,0 +1,61 @@
+package com.korit.servlet_study.ch11.dao;
+
+import com.korit.servlet_study.ch11.entity.Department;
+import com.korit.servlet_study.ch11.util.DBConnectionMgr;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class DepartmentDao {
+    // Dap는 보통 싱글톤으로 제작해야함
+    private final DBConnectionMgr mgr;
+
+    Connection con = null;
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    public List<Department> findAll() {
+        List<Department> departments = new ArrayList<>();
+
+        try {
+            con = mgr.getConnection();
+            String sql = """
+                    select
+                        department_id,
+                        department_code,
+                        department_name
+                    from
+                        department_tb
+                    order by 
+                        department_id
+                    """;
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Department department = Department.builder()
+                        .departmentId(rs.getInt("department_id"))
+                        .departmentCode(rs.getString("department_code"))
+                        .departmentName(rs.getString("department_name"))
+                        .build();
+
+                departments.add(department);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            mgr.freeConnection(con, ps, rs);
+        }
+        return departments;
+    }
+
+}
+
+
+
---
+++ src/main/java/com/korit/servlet_study/ch11/entity/Department.java
@@ -0,0 +1,4 @@
+package com.korit.servlet_study.ch11.entity;
+
+public class Department {
+}
